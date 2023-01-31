08d31bb3e5ad23b10d5e662faee92a82fa859fe0
Dan Nguyen <nguyen.danimal@gmail.com>
2023-01-31 10:51:28 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Configuration, OpenAIApi } from 'openai'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
