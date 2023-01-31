eea82101a0d62f2ea7590dd49b152d7f8e668877
Dan Nguyen <nguyen.danimal@gmail.com>
2023-01-31 11:16:10 -0600

    generate image on the console

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,24 @@ import { Configuration, OpenAIApi } from 'openai'
 import './App.css'
 
 function App() {
+  const configuration = new Configuration({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  })
+
+  const openai = new OpenAIApi(configuration)
+
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      prompt: 'The cliffs of Dover',
+      n: 1,
+      size: "1024x1024"
+    })
+    console.log(response.data.data[0].url)
+  }
 
   return (
-    <div className="App">
+    <div>
+      <button onClick={generateImage}>Generate</button>
     </div>
   )
 }
